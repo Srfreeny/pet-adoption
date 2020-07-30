dfcb1aee3531d1c0906293fd4c93abda2d455ce2
Srfreeny <srfreeny@comcast.net>
2020-07-29 20:56:25 -0500

    got the buttons to filter based on the type cateogry in the pet objects

--- pets.js
+++ pets.js
@@ -118,13 +118,13 @@ const pets = [
       type: "cat",
       imageUrl: "http://www.funnycatsite.com/pictures/Close_Up_Yawn.jpg"
     },
-    {
-      name: "Salem",
-      color: "Poop-Colored",
-      specialSkill: "Proficient in air guitar",
-      type: "dino",
-      imageUrl: "https://www.nation.co.ke/image/view/-/4078922/highRes/1742693/-/maxw/600/-/1453yvh/-/DINO.jpg"
-    },
+    // {
+    //   name: "Salem",
+    //   color: "Poop-Colored",
+    //   specialSkill: "Proficient in air guitar",
+    //   type: "dino",
+    //   imageUrl: "https://www.nation.co.ke/image/view/-/4078922/highRes/1742693/-/maxw/600/-/1453yvh/-/DINO.jpg"
+    // },
     {
       name: "Callie",
       color: "Blue",
@@ -139,20 +139,20 @@ const pets = [
       type: "cat",
       imageUrl: "http://www.funnycatsite.com/pictures/Lazy_White_Cat.jpg"
     },
-    {
-      name: "Miss kitty",
-      color: "Red",
-      specialSkill: "Owns a Nintendo Power Glove.",
-      type: "dino",
-      imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/2/12/enhanced/webdr15/anigif_enhanced-29802-1446485228-10.gif?crop=250:165;0,0&downsize=715"
-    },
-    {
-      name: "Snuggles",
-      color: "Orange",
-      specialSkill: "Is comfortable with jokes about his receding hairline.",
-      type: "cat",
-      imageUrl: "http://funnyanimalphoto.com/wp-content/uploads/2013/08/cat_caught_mouse_thegatewaypundit.jpg"
-    },
+    // {
+    //   name: "Miss kitty",
+    //   color: "Red",
+    //   specialSkill: "Owns a Nintendo Power Glove.",
+    //   type: "dino",
+    //   imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/2/12/enhanced/webdr15/anigif_enhanced-29802-1446485228-10.gif?crop=250:165;0,0&downsize=715"
+    // },
+    // {
+    //   name: "Snuggles",
+    //   color: "Orange",
+    //   specialSkill: "Is comfortable with jokes about his receding hairline.",
+    //   type: "cat",
+    //   imageUrl: "http://funnyanimalphoto.com/wp-content/uploads/2013/08/cat_caught_mouse_thegatewaypundit.jpg"
+    // },
     {
       name: "Buddy",
       color: "Red",
@@ -167,13 +167,13 @@ const pets = [
       type: "dog",
       imageUrl: "http://www.dogbreedplus.com/dog_breeds/images/basset-hound-4.jpg"
     },
-    {
-      name: "Salem",
-      color: "Red",
-      specialSkill: "Knows the words to 4 rap songs.",
-      type: "cat",
-      imageUrl: "http://funbk.s3.amazonaws.com/wp-content/uploads/2016/06/funny-cat-video-which-will-make-you-laugh-louder.jpg"
-    },
+    // {
+    //   name: "Salem",
+    //   color: "Red",
+    //   specialSkill: "Knows the words to 4 rap songs.",
+    //   type: "cat",
+    //   imageUrl: "http://funbk.s3.amazonaws.com/wp-content/uploads/2016/06/funny-cat-video-which-will-make-you-laugh-louder.jpg"
+    // },
     {
       name: "Bubba",
       color: "Yellow",
@@ -201,22 +201,49 @@ const pets = [
       specialSkill: "Gives hugs with appropriate pressure and for the right length of time.",
       type: "cat",
       imageUrl: "http://img.izismile.com/img/img2/20090219/cats_02.jpg"
-    },
-    {
-      name: "Lucy",
-      color: "Red",
-      specialSkill: "Doesn’t get weirded out by the word “moist.”",
-      type: "dino",
-      imageUrl: "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119"
     }
+    // {
+    //   name: "Lucy",
+    //   color: "Red",
+    //   specialSkill: "Doesn’t get weirded out by the word “moist.”",
+    //   type: "dino",
+    //   imageUrl: "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119"
+    // }
   ];
+
+  const handleButtonClick = (e) => {
+    const buttonId = e.target.id;
+    const selectedPets = [];
+  
+  for (let i = 0; i < pets.length; i++) {
+    if (pets[i].type === buttonId) {
+      selectedPets.push(pets[i]);
+    }
+  }
+  const bodySelector = document.querySelector('body');
+
+  if (buttonId === 'cat') {
+      bodySelector.style.backgroundColor = 'red';
+  } else if (buttonId === 'dog') {
+      bodySelector.style.backgroundColor = 'blue';
+  } else if (buttonId === 'dino') {
+      bodySelector.style.backgroundColor = 'yellow';
+  } else {
+      bodySelector.style.backgroundColor = 'black';
+  }
+  if (buttonId === 'All' || buttonId === e.currentTarget.id){
+    buildPetCards(pets);
+  } else {
+    buildPetCards(selectedPets);
+  }
+  }
 
 const printToDom =(divID, textToPrint) => {
     const selectedDiv = document.getElementById(divID);
     selectedDiv.innerHTML = textToPrint;
 }
 
-const buildPetCards = () => {
+const buildPetCards = (pets) => {
     let domString = "";
     for (let i = 0; i < pets.length; i++) {
         domString += `<div class="animal">`
@@ -230,26 +257,40 @@ const buildPetCards = () => {
     printToDom('pets', domString);
 }
 
-buildPetCards();
 
-let catsBtn = document.getElementById("catz")
-let dogsBtn = document.getElementById("dogz")
-let dinosBtn = document.getElementById("dinoz")
+const buttonEvents = () => {
+  document.querySelector('#buttons').addEventListener('click', handleButtonClick);
+}
 
-catz.addEventListener('click', buttonClicked);
-function buttonClicked(ev) {
-    console.log(ev.type, ev.target, ev.currentTarget)
+const init = () => {
+  buttonEvents();
+  buildPetCards(pets);
+}
 
-};
+init();
 
-dogz.addEventListener('click', buttonClicked);
-function buttonClicked(ev) {
-    console.log(ev.type, ev.target, ev.currentTarget)
+// let catsBtn = document.getElementById("cat")
+// let dogsBtn = document.getElementById("dog")
+// let dinosBtn = document.getElementById("dino")
 
-};
+// cat.addEventListener('click', buttonClicked);
+// function buttonClicked(ev) {
+//     console.log(ev.type, ev.target, ev.currentTarget)
 
-dinoz.addEventListener('click', buttonClicked);
-function buttonClicked(ev) {
-    console.log(ev.type, ev.target, ev.currentTarget)
+// };
 
-};
+// dog.addEventListener('click', buttonClicked);
+// function buttonClicked(ev) {
+//     console.log(ev.type, ev.target, ev.currentTarget)
+
+// };
+
+// dino.addEventListener('click', buttonClicked);
+// function buttonClicked(ev) {
+//     console.log(ev.type, ev.target, ev.currentTarget)
+
+// };
+
+
+
+
